3cc4415c96395cfb3b8a4c9abc5c40f3165a1526
prashi546 <prashi546@gmail.com>
2019-08-14 13:41:02 +0530

    Third commit with edit file and created new class for a repository

--- JarDemo/src/Demo.java
+++ JarDemo/src/Demo.java
@@ -5,6 +5,8 @@ public class Demo {
 	public static void main(String[] args) {
 		Calc C = new Calc();
 		C.add(2, 6);
+		System.out.println("Here adding two number");
 		//System.out.println(C);
+		//This is a another comment
 	}
 }
